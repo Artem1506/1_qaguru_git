b923079a97d27a6aa941036a2cc538d512055c85
vaulin1506 <vaulin1506@gmai.com>
2023-02-06 19:18:23 +0100

    switch to UC

--- src/com/vaulin1506/Main.java
+++ src/com/vaulin1506/Main.java
@@ -6,7 +6,7 @@ public class Main {
         System.out.println("Hello world");
         System.out.println("new line");
         System.out.println("slow branch");
-        System.out.println("replay");
+        System.out.println("REPLAY");
     }
 }
 
